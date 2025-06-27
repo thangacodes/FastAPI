e0bc9f96444726709c8e793a6c80f3fdde2d37d1
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2025-06-27 16:47:31 +0530

    updated

--- macapp_check.py
+++ macapp_check.py
@@ -1,24 +1,20 @@
-from fastapi import FastAPI
-from fastapi.responses import HTMLResponse, PlainTextResponse
 import subprocess
+from fastapi import FastAPI
+from fastapi.responses import PlainTextResponse, HTMLResponse
 
 app = FastAPI()
+
 @app.get("/", response_class=HTMLResponse)
 def root_page():
     html_content = """
-    <body style="background-color: skyblue;">
-    <br></br>
-    <h1>FastAPI:</h1>
-      <h2>Select an option:</h2>
-      <ul>
-        <li><a href="/root_page" target="_blank">root_page</a></li>
-        <li><a href="/pyv" target="_blank">pyv</a></li>
-        <li><a href="/pkv" target="_blank">pkv</a></li>
-        <li><a href="/tfv" target="_blank">tfv</a></li>
-      </ul>
-      <br></br>
-      <h3> made with interest and love ❤️ by Thangacodes..</h3>
-    </body>
+    <h2>Select an option:</h2>
+    <ul>
+      <li><a href="/root_page">root_page</a></li>
+      <li><a href="/ansv">ansv</a></li>
+      <li><a href="/pyv">pyv</a></li>
+      <li><a href="/pkv">pkv</a></li>
+      <li><a href="/tfv">tfv</a></li>
+    </ul>
     """
     return html_content
 
@@ -31,15 +27,23 @@ def root_text():
     except Exception as error:
         return f"Error: {str(error)}"
 
-@app.get("/tfv", response_class=PlainTextResponse)
-def get_tf_version():
-    print("Checking terraform version")
+@app.get("/ansv", response_class=PlainTextResponse)
+def get_ansible_version():
+    print("Checking ansible version")
     try:
-        result = subprocess.run(["terraform", "--version"], capture_output=True, text=True, check=True)
+        result = subprocess.run(["ansible", "--version"], capture_output=True, text=True, check=True)
         return result.stdout.strip()
     except subprocess.CalledProcessError as error:
-        return f"Error getting terraform version:\n{str(error)}"
-
+        return f"Error getting python version:\n{str(error)}"
+        
+@app.get("/pkv", response_class=PlainTextResponse)
+def get_pkr_version():
+    print("Checking packer version")
+    try:
+        result = subprocess.run(["packer", "--version"], capture_output=True, text=True, check=True)
+        return result.stdout.strip()
+    except subprocess.CalledProcessError as error:
+        return f"Error getting packer version:\n{str(error)}"
 @app.get("/pyv", response_class=PlainTextResponse)
 def get_py_version():
     print("Checking python version")
@@ -49,11 +53,11 @@ def get_py_version():
     except subprocess.CalledProcessError as error:
         return f"Error getting python version:\n{str(error)}"
 
-@app.get("/pkv", response_class=PlainTextResponse)
-def get_pkr_version():
-    print("Checking packer version")
+@app.get("/tfv", response_class=PlainTextResponse)
+def get_tf_version():
+    print("Checking terraform version")
     try:
-        result = subprocess.run(["packer", "--version"], capture_output=True, text=True, check=True)
+        result = subprocess.run(["terraform", "--version"], capture_output=True, text=True, check=True)
         return result.stdout.strip()
     except subprocess.CalledProcessError as error:
-        return f"Error getting packer version:\n{str(error)}"
+        return f"Error getting terraform version:\n{str(error)}"
